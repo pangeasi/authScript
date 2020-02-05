57bd49d0f1929f885bc01cdd18e4050112434a58
Daniel <danielb@devanddel.com>
2020-02-05 07:55:42 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import * as QrCode from 'qrcode-reader'
+import {read} from 'jimp'
+import {readdirSync, readFileSync} from 'fs'
+import * as path from 'path'
+import * as totp from "totp-generator";
+
+const pathQR = (__dirname.split('src')[0] + path.normalize('/putYourQRcodeHere'))
+const qrImage = readdirSync(pathQR)
+if (qrImage.length == 0) {
+    console.log('guarda tu qr en formato imagen en la carpeta putYourQRcodeHere')
+    process.exit()
+}
+const qrBuffer = readFileSync(`${pathQR}${path.normalize(`/${qrImage[0]}`)}`)
+
+const resultQR = (): Promise<String> => {
+    return new Promise((resolve) => {
+        read(qrBuffer, (err, img) => {
+            img.resize(800,800)
+            if(err) console.log('ERR:', err)
+            let qr = new QrCode()
+            qr.callback = (err, value) => {
+                if(err){
+                    console.log('ERR QR:', err)
+                }else{
+                    resolve(value.result)
+                }
+            }
+            
+            qr.decode(img.bitmap)
+        })
+    })
+}
+
+const printTotp = async () => {
+    if(!process.argv[2]) console.log('pasa tu pass como argumento, ejemplo: npm start [password]')
+    const secret = (await resultQR()).split('&')[1].replace('secret=','')
+    console.log(`${process.argv[2] || '[aqui irá tu pass] '}${totp(secret)}`)
+}
+
+printTotp()
+setInterval( ()=>{
+    printTotp()
+}, 30000)
+
+
